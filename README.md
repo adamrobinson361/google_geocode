904d78353cbd54106bc8686c195682320f56a740
Adam Robinson <adamrobinson361@gmail.com>
2020-03-14 12:07:10 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# google_geocode
+
+This is a very small R Shiny web app that utilises that wraps around the Google Maps Geocode API to provide an interface for searching for addresses and returning longitude/latitude.
---
+++ app.R
@@ -0,0 +1,53 @@
+## app.R ##
+library(shinydashboard)
+library(mapsapi)
+library(leaflet)
+library(xml2)
+library(DT)
+
+key_txt <- Sys.getenv("geocode_key")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Google Geocode"),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(textInput("input_text_box", "Address", "Aston Academy, Aughton Road, Swallownest, Sheffield, South Yorkshire, S26 4SF"), width = 10),
+      box(actionButton("do", "Search"), width = 2)
+    ),
+    fluidRow(
+      box(leafletOutput("mymap"), width = 12)
+    ),
+    fluidRow(
+      box(dataTableOutput("mytable"), width = 12)
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  observeEvent(input$do, {
+    
+    doc = mp_geocode(addresses = input$input_text_box, region = "gb", key = key_txt)
+    
+    pnt = mp_get_points(doc)
+    
+    output$mymap <- renderLeaflet({
+      
+      leaflet() %>%
+        addTiles(urlTemplate = "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", attribution = 'Google', options = providerTileOptions(noWrap = TRUE)) %>%
+        addMarkers(data = pnt)
+    })
+    
+    output$mytable = DT::renderDataTable({
+      
+      pnt
+    })
+     
+  }
+  )
+
+}
+
+shinyApp(ui, server)
