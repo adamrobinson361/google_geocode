72aed0521aaf67b62469c61e7dbd1b21df328527
Adam Robinson <adamrobinson361@gmail.com>
2020-03-14 12:10:35 +0000

    Add deploy script

---
+++ deploy.R
@@ -0,0 +1,20 @@
+# Create conditional app name based on branch
+
+app_name <- if (Sys.getenv("TRAVIS_BRANCH") == "master") {
+  "google_geocode"
+} else if (Sys.getenv("TRAVIS_BRANCH") == "develop") {
+  "google_geocode_dev"
+}
+
+# Set account info
+rsconnect::setAccountInfo(
+  name = "adamrobinson361",
+  token = Sys.getenv("SHINYAPPS_TOKEN"),
+  secret = Sys.getenv("SHINYAPPS_SECRET")
+)
+
+# Print name to console
+print(app_name)
+
+# Deploy
+rsconnect::deployApp(appName = app_name)
